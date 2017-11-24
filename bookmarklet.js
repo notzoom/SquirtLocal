88d3a1e98372152915aed9bcdee937bb3f5a6757
RStudios <muffman9@hotmail.co.uk>
2017-11-24 00:06:53 +0000

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,18 +1,13 @@
-(function(){
+javascript:(function(){
   if(window.sq){
      window.sq.closed && window.document.dispatchEvent(new Event('squirt.again'));
   } else {
     window.sq = {};
     window.sq.userId = '--squirtUser--';
     s = document.createElement('script');
-    s.src = 'http://www.squirt.io/bm/squirt.js';
+    s.src = 'https://rawgit.com/rstudios/SquirtLocal/master/squirt.js';
     s.s = window.location.search;
     s.idx = s.s.indexOf('sq-dev');
-    if(s.idx != -1){
-      s.ampIdx = s.s.indexOf('&', s.idx);
-      s.host = s.s.substring(s.idx + 7, s.ampIdx == -1 ? s.s.length : s.ampIdx);
-      s.src = 'http://' + (s.host ? s.host : 'localhost') + ':4000/bm/squirt.js';
-    }
     document.body.appendChild(s);
   }
 })();
